61459b8ea603511583db2d35c79da0f844f430f1
PrashantDas <78946070+PrashantDas@users.noreply.github.com>
2023-07-28 22:22:40 +0530

    Update settings.py

--- projecthospital/settings.py
+++ projecthospital/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'z5WkqRWp5xMV95ztCyrc',
+        'PASSWORD': os.environ["my_abundant_glove"],
         'HOST': 'containers-us-west-188.railway.app',
         'PORT': '6495',
     }
@@ -139,10 +139,8 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 # AWS Bucket
-# AWS_ACCESS_KEY_ID = os.environ["MY_AWS_ACCESS_KEY_ID"]
-AWS_ACCESS_KEY_ID = "AKIAWHX3SYZSCEU5AIPH"
-# AWS_SECRET_ACCESS_KEY = os.environ["MY_AWS_SECRET_ACCESS_KEY"]
-AWS_SECRET_ACCESS_KEY = "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"
+AWS_ACCESS_KEY_ID = os.environ["MY_AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["MY_AWS_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = 'everything-django'
 AWS_S3_REGION_NAME = 'ap-south-1'
 AWS_QUERYSTRING_AUTH = False
